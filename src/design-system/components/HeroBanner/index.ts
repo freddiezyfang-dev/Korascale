f502d1784be5026a99d661ddcab5f9595b2bd723
Freddie Fang <freddiefang@FreddiedeMacBook-Air.local>
2025-10-16 00:37:56 +0800

    fix: 修复 HeroBanner 模块导入路径
    - 修正相对路径从 ../../../ 到 ../../../../
    - 确保能正确找到 HeroBanner 组件

--- src/design-system/components/HeroBanner/index.ts
+++ src/design-system/components/HeroBanner/index.ts
@@ -1,5 +1,5 @@
-export { default as HeroBanner } from '../../../components/sections/HeroBanner';
-export type { HeroBannerProps } from '../../../components/sections/HeroBanner';
+export { default as HeroBanner } from '../../../../components/sections/HeroBanner';
+export type { HeroBannerProps } from '../../../../components/sections/HeroBanner';
 
 
 
